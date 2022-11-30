b0e5e383976c1f841661446ae6409e2e44591cce
Alassane GUEYE <84050177+alassanegu@users.noreply.github.com>
2022-11-30 15:28:36 +0100

    first commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,15 +2,29 @@
 /** Une classe liste FIFO. */
 case class Queue[T](in:List[T] = Nil, out:List[T] = Nil) {
   /** Ajoute un élément `x` en tête. */
-  def enqueue(x:T):Queue[T] = ???
+  def enqueue(x:T):Queue[T] = {
+
+
+  }
   /** Retire le dernier élément. */
-  def dequeue():(T,Queue[T]) = ???
+  def dequeue():(T,Queue[T]) = {
+
+
+  }
   /** Accès au premier élément, s'il existe (dernier élément entré). */
-  def headOption():Option[T] = ???
+  def headOption():Option[T] = {
+
+
+  }
   /** Vrai si la liste est vide. */
   def isEmpty:Boolean = in.isEmpty && out.isEmpty
 }
-object Main {
+
+object fifo {
+
+}
+
+object testFifo {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
   }
